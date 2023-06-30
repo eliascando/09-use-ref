bf05003f0c20c9ce5949c50af3b2c405941c3465
Elias Esteban Cando Jaime <elias.candoj@ug.edu.ec>
2023-06-30 16:22:35 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Ejemplo } from './components/Ejemplo'
+import { Formulario } from './components/Formulario'
+
+function App() {
+  return (
+    <div>
+      <Formulario/>
+      <hr/>
+      <Ejemplo/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Ejemplo.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useRef, useState } from "react"
+
+export const Ejemplo = () => {
+    const [numeroSaludo, setNumeroSaludo] = useState(0);
+
+    const saludosEnCola = useRef(numeroSaludo);
+
+    useEffect(()=>{
+        saludosEnCola.current = numeroSaludo;
+        setTimeout(()=>{
+            console.log("Mensajes en cola: " + saludosEnCola.current);
+        }, 2000)
+    },[numeroSaludo]);
+
+    const enviarSaludo = () => {
+        setNumeroSaludo(numeroSaludo + 1);
+    }
+
+  return (
+    <div>
+        <h1>Ejemplo con useRef</h1>
+        <h2>Saludos enviados: {numeroSaludo}</h2>
+        <button onClick={enviarSaludo}>Enviar Saludo!</button>
+
+    </div>
+  )
+}
